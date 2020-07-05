0c1af02423a155b4bd4589fb11c522955d2a88eb
Jose Martinez <josealbertomartinez.vi@gmail.com>
2020-07-05 16:57:17 -0500

    Componente inicial GiftExpertApp

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GiftExpertApp</h2>
+            <hr/>
+        </>
+    );
+}
+
+export default GifExpertApp;
