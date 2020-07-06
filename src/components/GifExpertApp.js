78fcfe9227555a694162546d8daa28d66b7bcf0f
Jose Martinez <josealbertomartinez.vi@gmail.com>
2020-07-05 19:36:46 -0500

    Agregada lista inicial de categorias y componente AddCategory para agregar una nueva categoria

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './addCategory/AddCategory';
 
 const GifExpertApp = () => {
+
+    // const categorias = ['One Puch Man', 'Samurai X', 'Dragon Ball'];
+
+    const [categorias, setCategories] = useState(['One Puch Man', 'Samurai X', 'Dragon Ball']);
+
     return (
         <>
             <h2>GiftExpertApp</h2>
             <hr/>
+            <AddCategory setCategories={ setCategories } />
+            <ol>
+                { 
+                    categorias.map( categoria => {
+                        return <li key = { categoria }>{ categoria }</li>
+                    } )
+                }
+            </ol>
         </>
     );
 }
